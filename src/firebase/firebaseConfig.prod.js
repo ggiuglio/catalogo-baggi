b40505861cbb4e4afedfd6607ad175476df41fc2
ggiuglio <ggiuglio@hotmail.com>
2019-10-05 12:59:26 +0200

    test-prod settings

---
+++ src/firebase/firebaseConfig.prod.js
@@ -0,0 +1,5 @@
+export const FirebaseConfig = {
+    apiKey: "AIzaSyDAVbKqR2S70HlSUBE39VrVk6msvoN2cyI",
+    authDomain: "prt-baggi.firebaseapp.com",
+    databaseURL: "https://prt-baggi.firebaseio.com/"
+  };
